1b330d900aab6e7a30068cc7bf67097760bd9796
carole tierney <caroleatierney@gmail.com>
2026-05-08 16:27:48 -0400

    fixed centering photo and overall padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Main from "./Components/Main"; // adjust path as needed
+import BCLogo from "./Images/BCLogo.png";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/about" element={<Main />} />
-      {/* Add more routes here */}
-    </Routes>
+    // <div
+    //   style={{ backgroundImage: `url(${BCLogo})` }}
+    //   className="bg-repeat min-h-screen"
+    // >
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/about" element={<Main />} />
+        {/* Add more routes here */}
+      </Routes>
+    // </div>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -6,48 +6,51 @@ import MPG_2 from "../Images/MPG_2.jpg";
 
 export default function About() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-black">
-      <div className="flex flex-row items-center justify-center h-screen bg-black">
-        <div className="w-[800px] h-[800px] p-5 mx-auto">
-          <img
-            className="object-cover border-16 border-white"
-            src={BCLogo}
-            alt="BC Logo"
-          />
-        </div>
-
-        <div
-          className="w-[800px] h-[800px] p-5 mx-auto"
-          style={{ color: "#DOBD49" }}
-        >
-          <a
-            href="https://www.youtube.com/watch?v=Np1iJRYXzUQ"
-            title="Fox News"
-            target="_blank"
-          >
+    <div>
+      <div className="flex flex-row md:flex-col items-center justify-center bg-black h-full p-10">
+        <div className="flex flex-row items-center justify-center">
+          <div className="w-1/2 h-1/2 p-5 mx-auto">
             <img
-              src={heDidIt}
-              alt="BC Field Goal"
               className="object-cover border-16 border-white"
+              src={BCLogo}
+              alt="BC Logo"
             />
-          </a>
-        </div>
-      </div>
+          </div>
 
-      <div className="flex flex-col items-center justify-center h-screen bg-black">
-        <div className="w-3/4 p-5 mx-auto">
-          <img
-            className="object-cover border-8 border-white"
-            src={MPG_1}
-            alt="xxx1"
-          />
+          <div className="w-1/2 h-1/2 p-10 mx-auto" style={{ color: "#DOBD49" }}>
+            <a
+              className="p-0"
+              href="https://www.youtube.com/watch?v=Np1iJRYXzUQ"
+              title="Fox News"
+              target="_blank"
+            >
+              <img
+                src={heDidIt}
+                alt="BC Field Goal"
+                className="border-16 mx-auto border-white"
+              />
+            </a>
+            <h1 className="text-white text-center text-md md:text-3xl">
+              Watch Fox News Coverage
+            </h1>
+          </div>
         </div>
-        <div className="w-1/3 p-5 mx-auto">
-          <img
-            className="object-cover border-8 border-white"
-            src={MPG_2}
-            alt="xxx2"
-          />
+
+        <div className="flex flex-row items-center justify-evenly bg-black pt-30">
+          <div className="w-2/3 mx-auto">
+            <img
+              className="border-8 border-white object-cover mx-auto w-7/8"
+              src={MPG_1}
+              alt="News Article"
+            />
+          </div>
+          <div className="w-1/3 mx-auto">
+            <img
+              className="border-8 border-white object-cover mx-auto w-3/4"
+              src={MPG_2}
+              alt="News Article Continued"
+            />
+          </div>
         </div>
       </div>
     </div>
