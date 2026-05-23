fe52f3271839eedbd00b398f6a83c530c0e66bc7
carole tierney <caroleatierney@gmail.com>
2026-05-22 21:06:02 -0400

    fix top div row

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -7,18 +7,18 @@ import MPG_2 from "../Images/MPG_2.jpg";
 export default function About() {
   return (
     <div className=" bg-black">
-      <div className="flex flex-col items-center justify-center h-full p-10">
-        <div className="flex flex-col md:flex-row items-center justify-center">
-          <div className="w-full md:w-1/2 h-1/2 p-5 mx-auto">
+      <div className="flex flex-col items-center h-full p-10">
+        <div className="flex flex-col w-full md:flex-row justify-evenly m-5">
+          <div className="w-full md:w-1/2 p-5 mx-auto">
             <img
-              className="object-cover border-16 border-white"
+              className="object-cover border-16 border-white mx-auto"
               src={BCLogo}
               alt="BC Logo"
             />
           </div>
 
           <div
-            className="w-full md:w-1/2 h-1/2 p-5 mx-auto"
+            className="w-full md:w-1/2 p-5 mx-auto"
             style={{ color: "#DOBD49" }}
           >
             <a
@@ -33,7 +33,7 @@ export default function About() {
                 className="border-16 mx-auto border-white"
               />
             </a>
-            <h1 className="text-white text-center text-md md:text-3xl">
+            <h1 className="text-white text-center text-md md:text-3xl p-10">
               Watch Fox News Coverage
             </h1>
           </div>
@@ -42,14 +42,14 @@ export default function About() {
         <div className="flex flex-col md:flex-row items-center justify-evenly p-5">
           <div className="w-full md:w-2/3 mx-auto">
             <img
-              className="border-8 border-white object-cover mx-auto w-7/8 m-5"
+              className="border-8 border-white object-cover mx-auto md:w-7/8"
               src={MPG_1}
               alt="News Article"
             />
           </div>
-          <div className="w-full md:w-1/3 mx-auto m-5">
+          <div className="w-full md:w-1/3 mx-auto m-5 pt-5">
             <img
-              className="border-8 border-white object-cover mx-auto w-3/4"
+              className="border-8 border-white object-cover mx-auto md:w-4/5"
               src={MPG_2}
               alt="News Article Continued"
             />
